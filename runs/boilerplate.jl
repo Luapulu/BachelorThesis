94968767d5b297478d923671fd79082a60d140e3
Paul Nemec <paul.nemec@tum.de>
2020-08-29 15:10:34 +0200

    Further Fixes

--- runs/boilerplate.jl
+++ runs/boilerplate.jl
@@ -1,6 +1,6 @@
 include("../common/common.jl")
 using YAML
 
-const PROJECT_CONFIG = YAML.load_file("../project-config.yaml")
-const CO56_MAGE_CONFIG = config["Co-56_MaGeHits"]
+const PROJECT_CONFIG = YAML.load_file("project-config.yaml")
+const CO56_MAGE_CONFIG = PROJECT_CONFIG["Co-56_MaGeHits"]
 const CO56_HIT_ITER = MaGeHitIter(CO56_MAGE_CONFIG["dirpath"], Regex(CO56_MAGE_CONFIG["pattern"]))
