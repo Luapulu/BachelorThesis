be5adbd79dbd0aa8d941575db9a78337259e70b5
Paul Nemec <paul.nemec@tum.de>
2020-08-29 18:27:41 +0200

    Added parserow function
    Also changed back config name

--- common/common.jl
+++ common/common.jl
@@ -21,3 +21,35 @@ function iterate(iter::MaGeHitIter, state)
     return rowvector[rowindex], (rowvector, fileindex, rowindex+1)
 end
 iterate(iter::MaGeHitIter) = iterate(iter::MaGeHitIter, (AbstractString[], 0, 1))
+
+struct MaGeHit
+    x::Float32
+    y::Float32
+    z::Float32
+    E::Float32
+    t::Float32
+    particleid::Int32
+    trackid::Int32
+    trackparentid::Int32
+end
+
+function parserow(row::AbstractString, xtal_length=1)
+    stringarr = split(row)
+    length(stringarr) !== 9 && return row
+
+    x =             parse(Float32, stringarr[3])
+    y =             parse(Float32, stringarr[1])
+    z =             parse(Float32, stringarr[2])
+    E =             parse(Float32, stringarr[4])
+    t =             parse(Float32, stringarr[5])
+    particleid =    parse(Int32, stringarr[6])
+    trackid =       parse(Int32, stringarr[7])
+    trackparentid = parse(Int32, stringarr[8])
+
+    # convert to detector coordinates [mm]
+    x = 10(x + 200)
+    y = 10y
+    z = -10z + 0.5xtal_length
+
+    return MaGeHit(x, y, z, E, t, particleid, trackid, trackparentid)
+end
--- runs/boilerplate.jl
+++ runs/boilerplate.jl
@@ -1,6 +1,6 @@
 include("../common/common.jl")
 using YAML
 
-const PROJECT_CONFIG = YAML.load_file("project-config.yaml")
-const CO56_MAGE_CONFIG = PROJECT_CONFIG["Co-56_MaGeHits"]
+const CONFIG = YAML.load_file("config.yaml")
+const CO56_MAGE_CONFIG = CONFIG["Co-56_MaGeHits"]
 const CO56_HIT_ITER = MaGeHitIter(CO56_MAGE_CONFIG["dirpath"], Regex(CO56_MAGE_CONFIG["pattern"]))
