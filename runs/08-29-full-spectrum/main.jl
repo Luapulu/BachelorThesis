a5059c6e197313b19d618d0fa9f08e0cb077d78b
Paul Nemec <paul.nemec@tum.de>
2020-08-31 23:29:48 +0200

    Final spectrum calculation

--- runs/08-29-full-spectrum/main.jl
+++ runs/08-29-full-spectrum/main.jl
@@ -1,5 +1,5 @@
-@time result = sum(filemap(CO56_HIT_FILES[1:8]) do file
+@time result = sum(filemap(CO56_HIT_FILES) do file
     calcfrequencies(calcenergy, file, 3000, (500, 3500))
 end)
 
-JLD.save("spectrum.jld", "spectrum", result)
+JLD.save("runs/08-29-full-spectrum/spectrum.jld", "spectrum", result)
