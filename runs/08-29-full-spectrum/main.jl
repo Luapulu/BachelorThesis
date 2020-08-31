2d101af80e036695330fa3063cdf4b898d967f71
Paul Nemec <paul.nemec@tum.de>
2020-08-31 22:45:06 +0200

    Fixes

--- runs/08-29-full-spectrum/main.jl
+++ runs/08-29-full-spectrum/main.jl
@@ -1,4 +1,4 @@
-@everywhere include("../boilerplate.jl")
+@everywhere include("runs/boilerplate.jl")
 
 @time result = sum(filemap(CO56_HIT_FILES[1:8]) do file
     calcfrequencies(calcenergy, file, 3000, (800, 4000))
