67a93f951949095034eaa9e5ab3285d8b48ac585
Paul Nemec <paul.nemec@tum.de>
2020-09-15 16:30:11 +0200

    Fix

--- runs/09-11-signals/converttojld.jl
+++ runs/09-11-signals/converttojld.jl
@@ -11,7 +11,9 @@ dir = realpath(joinpath(dirname(pathof(MaGeAnalysis)), "..", "runs", "09-11-sign
 configpath = realpath(joinpath(dir, "GWD6022_01ns.config"))
 @everywhere init_setup($configpath)
 
+mkdir(joinpath(dir, "events"))
+
 @time eventstojld(
     "/mnt/e15/comellato/results4Paul/GWD6022_Co56_side50cm/DM",
-    joinpath(dir, events)
+    joinpath(dir, "events")
 )
