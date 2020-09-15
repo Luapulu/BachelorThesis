af71fca1b53f497b6513457bdc4716d3ceefaf11
Paul Nemec <paul.nemec@tum.de>
2020-09-15 16:54:54 +0200

    Added siggen script

---
+++ runs/09-11-signals/siggen.jl
@@ -0,0 +1,20 @@
+using Distributed
+nworkers() < 16 && addprocs(17 - nprocs())
+
+@everywhere import Pkg
+@everywhere Pkg.activate(".")
+@everywhere Pkg.instantiate()
+@everywhere using MaGeAnalysis
+
+dir = realpath(joinpath(dirname(pathof(MaGeAnalysis)), "..", "runs", "09-11-signals"))
+
+configpath = realpath(joinpath(dir, "GWD6022_01ns.config"))
+@everywhere init_setup($configpath)
+
+mkdir(joinpath(dir, "signals"))
+
+get_signals(
+    e -> 800 < energy(e) < 4000
+    joinpath(dir, "events"),
+    joinpath(dir, "signals")
+)
