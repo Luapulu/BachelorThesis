4c0f4ee9b2418ebc7aa567626339234dc4d3a9e4
Paul Nemec <paul.nemec@tum.de>
2020-09-15 16:54:33 +0200

    Formatting

--- src/get_signals.jl
+++ src/get_signals.jl
@@ -1,10 +1,10 @@
 ## Generating signals for events and hits ##
 
-function get_signal!(pulse::DenseVector{Float32}, location::NTuple{3, T} where T)
+function get_signal!(pulse::DenseVector{Float32}, location::NTuple{3,T} where T)
     get_signal!(pulse, SETUP, location)
 end
 
-function get_signal(location::NTuple{3, T} where T)
+function get_signal(location::NTuple{3,T} where T)
     get_signal!(SETUP, location)
 end
 
@@ -20,7 +20,7 @@ get_signal(h::MaGeHit) = get_signal((h.x, h.y, h.z))
 function get_signal!(
     finpulse::DenseVector{Float32},
     event::MaGeEvent,
-    pulse::DenseVector{Float32} = Vector{Float32}(undef, SETUP.ntsteps_out)
+    pulse::DenseVector{Float32} = Vector{Float32}(undef, SETUP.ntsteps_out),
 )
     for hit in event
         get_signal!(pulse, hit)
@@ -34,10 +34,10 @@ get_signal(e::MaGeEvent) = get_signal!(zeros(Float32, SETUP.ntsteps_out), e)
 ## Generating signals for multiple events ##
 
 function get_signals!(
-    sigs::Vector{Union{Missing, Vector{Float32}}},
+    sigs::Vector{Union{Missing,Vector{Float32}}},
     events::Vector{MaGeEvent};
     replace::Bool = false,
-    info_every::Int = 100
+    info_every::Int = 100,
 )
     for (i, e) in enumerate(events)
         if ismissing(sigs[e.fileindex]) || replace
@@ -47,17 +47,17 @@ function get_signals!(
             @info "Worker $(myid()) got signals for $i out of $(length(events)) of events"
         end
     end
-    @info "Worker $(myid()) got signals for all given events" signals=sigs
+    @info "Worker $(myid()) got signals for all given events" signals = sigs
     return sigs
 end
 
 function get_signals(events::Vector{MaGeEvent}, l::Integer; kwargs...)
-    return get_signals!(Vector{Union{Missing, Vector{Float32}}}(missing, l), events; kwargs...)
+    return get_signals!(Vector{Union{Missing,Vector{Float32}}}(missing, l), events; kwargs...)
 end
 
 ## Saving and loaing signals ##
 
-save_signals(sigs::Vector{Union{Missing, Vector{Float32}}}, path::String) =
+save_signals(sigs::Vector{Union{Missing,Vector{Float32}}}, path::String) =
     savejld2(sigs, "signals", path)
 
 get_signals(path::String) = loadjld2("signals", path)
@@ -69,7 +69,12 @@ function getsignalpath(path::String, dir::String)
     savepath = joinpath(dir, splitext(f)[1] * "_signals" * ".jld2")
 end
 
-function get_signals(condition, eventpaths::Vector{String}, savedir::String; kwargs...)
+function get_signals(
+    condition,
+    eventpaths::Union{String,Vector{String}},
+    savedir::String;
+    kwargs...,
+)
     filemap(eventpaths) do path
         events = get_events(path)
         sigs = get_signals(filter(condition, events), length(events); kwargs...)
@@ -82,7 +87,12 @@ function get_signals(condition, eventpaths::Vector{String}, savedir::String; kwa
     end
 end
 
-function get_signals!(condition, eventpaths::Vector{String}, savedir::String; kwargs...)
+function get_signals!(
+    condition,
+    eventpaths::Union{String,Vector{String}},
+    savedir::String;
+    kwargs...,
+)
     filemap(eventpaths) do path
         sigpath = getsignalpath(path, savedir)
         sigs = get_signals(sigpath)
