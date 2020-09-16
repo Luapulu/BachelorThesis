43ce4b4b811cf06e1f87046a51a07c3429d4db11
Paul Nemec <paul.nemec@tum.de>
2020-09-16 18:42:26 +0200

    Fix

--- runs/09-11-signals/converttojld.jl
+++ runs/09-11-signals/converttojld.jl
@@ -12,11 +12,12 @@ configpath = realpath(joinpath(dir, "GWD6022_01ns.config"))
 
 event_dir = "/mnt/e15/comellato/results4Paul/GWD6022_Co56_side50cm/DM"
 
-@distributed for path in readdir(event_dir, join=true)
-    if occursin(r".root.hits$", path)
-        save_path = joinpath(dir, "events", split(splitdir(path)[end], ".", limit=2)[1] * ".jld2")
-        save_events(MaGeEvent[e for e in eachevent(eventpath)], save_path)
-        @info "Converted $path"
-    end
+event_paths = filter(p -> occursin(r".root.hits$", path), readdir(event_dir, join=true))
+@info "Got event paths" event_paths
+
+@distributed for path in event_paths
+    save_path = joinpath(dir, "events", split(splitdir(path)[end], ".", limit=2)[1] * ".jld2")
+    save_events(MaGeEvent[e for e in eachevent(eventpath)], save_path)
+    @info "Converted $path"
     nothing
 end
