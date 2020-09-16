27cfbef0cd9b74fa53219b2d4b7c01ef4169fc9d
Paul Nemec <paul.nemec@tum.de>
2020-09-16 21:51:54 +0200

    Added script to convert

---
+++ runs/09-16-AoE-normalise/convert.jl
@@ -0,0 +1,19 @@
+using Distributed
+nworkers() < 12 && addprocs(13 - nworkers())
+
+@everywhere import Pkg
+@everywhere Pkg.activate(".")
+@everywhere using MaGeSigGen
+
+dir = realpath(joinpath(dirname(pathof(MaGeSigGen)), "..", "runs", "09-16-AoE-normalise"))
+
+configpath = realpath(joinpath(dir, "GWD6022_01ns.config"))
+@everywhere init_detector($configpath)
+
+event_dir = "/mnt/e15/comellato/results4Paul/GWD6022_Co56_side50cm/DM"
+
+event_paths = filter(p -> occursin(r".root.hits$", p), readdir(event_dir, join=true))
+
+pmap(event_paths) do path
+    events_to_jld(path, joinpath(dir, "events"))
+end
