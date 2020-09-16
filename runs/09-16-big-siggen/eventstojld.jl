4a8437508ef421232badd7a53ce491d82cdf2877
Paul Nemec <paul.nemec@tum.de>
2020-09-16 16:21:55 +0200

    Signal generation in Heidelberg

---
+++ runs/09-16-big-siggen/eventstojld.jl
@@ -0,0 +1,21 @@
+using Distributed
+worker_num = 10
+nprocs() <= worker_num && addprocs(1 + worker_num - nprocs())
+
+@everywhere import Pkg
+@everywhere Pkg.activate(".")
+@everywhere using MaGeSigGen
+
+dir = realpath(joinpath(dirname(pathof(MaGeSigGen)), "..", "runs", "09-16-big-siggen"))
+
+configpath = joinpath(dir, "GWD6022_01ns.config")
+@everywhere init_detector($configpath)
+
+event_paths = ????
+
+!isdir(joinpath(dir, "events")) && mkdir(joinpath(dir, "events"))
+
+@distributed for path in event_paths
+    save_path = joinpath(dir, "events", split(splitdir(path)[end], ".", limit=2)[1] * ".jld2")
+    save_events(MaGeEvent[e for e in eachevent(eventpath)], save_path)
+end
---
+++ runs/09-16-big-siggen/siggen.jl
@@ -0,0 +1,22 @@
+using Distributed
+worker_num = 200
+nprocs() <= worker_num && addprocs(1 + worker_num - nprocs())
+
+@everywhere import Pkg
+@everywhere Pkg.activate(".")
+@everywhere using MaGeSigGen
+
+dir = realpath(joinpath(dirname(pathof(MaGeSigGen)), "..", "runs", "09-16-big-siggen"))
+
+configpath = joinpath(dir, "GWD6022_01ns.config")
+@everywhere init_detector($configpath)
+
+!isdir(joinpath(dir, "signals")) && mkdir(joinpath(dir, "signals"))
+
+pmap(readdir(joinpath(dir, "events"), join=true)) do path
+    @info "Worker $(myid()) working on $(splitdir(path)[end])"
+    events = getevents(path)
+    filtered_events = filter(e -> (800 < energy(e) < 4000), events)
+    signals = get_signals(filtered_events, length(events))
+    save_signals(signals, joinpath(dir, "signals", splitdir(path)[end]))
+end
