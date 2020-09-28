bd9197a28dc2160236d428b8fd7cccabd460d295
Paul Nemec <paul.nemec@tum.de>
2020-09-28 08:37:39 +0200

    Simplified dependencies

--- src/MaGeSigGen.jl
+++ src/MaGeSigGen.jl
@@ -1,24 +1,15 @@
 module MaGeSigGen
 
-using Distributed, JLD, MJDSigGen, Logging, Parsers, DSP, Distributions
-using MJDSigGen:
-    Struct_MJD_Siggen_Setup, signal_calc_init, fieldgen
+using MaGe, Distributed, JLD, Logging, DSP
+using MJDSigGen: Struct_MJD_Siggen_Setup, outside_detector
+using Distributions: Normal
 
+import MJDSigGen: get_signal!
 import Base
-import MJDSigGen: get_signal!, outside_detector
 import JLD: save
 
 # Detector setup
-export setup, init_setup, with_group_effects, outside_detector, fieldgen
-
-# Hits
-export Hit, location, energy, time, particleid, trackid, trackparentid
-
-# Events
-export Event, hits, hitcount, eventnum, primarycount
-
-# Event files
-export load_events
+export with_group_effects!, todetcoords, todetcoords!
 
 # Signal generation
 export SignalDict, signals, get_signal, get_signal!, get_signals, get_signals!, save, load_signals
@@ -28,8 +19,6 @@ export getA, drift_time, charge_cloud_size, getδτ, apply_group_effects, set_no
 
 
 include("setup.jl")
-include("Event.jl")
-include("event-files.jl")
 include("get_signals.jl")
 include("signal-processing.jl")
 
