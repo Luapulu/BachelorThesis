a90ca93b51e6413f81416d9b765a75076a2003da
Paul Nemec <paul.nemec@tum.de>
2020-09-28 09:42:05 +0200

    Updated script

--- runs/09-24-big-siggen2/main.jl
+++ runs/09-24-big-siggen2/main.jl
@@ -1,19 +1,19 @@
-using MaGeSigGen
+using MaGeSigGen, MJDSigGen
 
 dir = realpath(joinpath(dirname(pathof(MaGeSigGen)), "..", "runs", "09-24-big_siggen2"))
 event_dir = "/mnt/e15/comellato/results4Paul/GWD6022_Co56_side50cm/DM"
 
 setup_path = joinpath(dir, "GWD6022_01ns.config")
-init_setup(setup_path)
+const setup = MJDSigGen.signal_calc_init(setup_path)
 
-compton_regions = [
+const compton_regions = [
     1070, 1105, 1130, 1187, 1220, 1300, 1420, 1480,
     1550, 1600, 1700, 1790, 1900, 2000, 2140, 2300,
     2400, 2560, 2640, 2710, 2800, 2850, 2900, 2970,
     3100, 3300
 ]
 
-dep_regions = [1577, 1988, 2180, 2232, 2251, 2429]
+const dep_regions = [1577, 1988, 2180, 2232, 2251, 2429]
 
 function event_filter(e)
     E = energy(e)
@@ -31,13 +31,22 @@ function getrawsignals(filenum)
     path = get_eventpath(filenum)
     @info "Working on $(splitdir(path)[end])"
 
-    filtered_events = Iterators.filter(event_filter, load_events(Event{Vector{Hit}}, path))
+    MaGe.loadstreaming(path) do stream
+        sgnls = SignalDict()
 
-    sgnls = get_signals(SignalDict, filtered_events)
+        for event in stream
+            if event_filter(event)
+                todetcoords!(event)
+                sgnls[event] = get_signal(setup, event)
+            end
+        end
 
-    save_path = joinpath(dir, "signals", split(splitdir(path)[end], '.')[1] * "_signals.jld")
+        save_path = joinpath(dir, "signals", split(splitdir(path)[end], '.')[1] * "_signals.jld")
 
-    save(save_path, sgnls)
-    @info "Saved signals to $(splitdir(save_path)[end])"
-    return nothing
+        save(save_path, sgnls)
+
+        @info "Saved signals to $(splitdir(save_path)[end])"
+    end
+
+    nothing
 end
