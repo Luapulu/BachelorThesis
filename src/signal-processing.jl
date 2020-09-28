a8c24b669fcf27b2747f3fc3a08565f4be657b51
Paul Nemec <paul.nemec@tum.de>
2020-09-28 08:38:20 +0200

    Added moving averages

--- src/signal-processing.jl
+++ src/signal-processing.jl
@@ -1,6 +1,6 @@
 getA(signal) = maximum(diff(signal))
 
-function drift_time(signal, lowthr, highthr; step_time_out=setup().step_time_out)
+function drift_time(signal, lowthr, highthr, step_time_out)
     lowthr > highthr && throw(ArgumentError("lowthr must be <= highthr"))
 
     minidx = 1
@@ -16,7 +16,7 @@ function drift_time(signal, lowthr, highthr; step_time_out=setup().step_time_out
     return (maxidx - minidx) * step_time_out
 end
 
-drift_time(signal; step_time_out=setup().step_time_out) = length(signal) * step_time_out
+drift_time(signal, step_time_out) = length(signal) * step_time_out
 
 """
     ch_cloud_size(energy)
@@ -27,12 +27,12 @@ Parameters A and B were obtained from MC of BSI inv-coax (evolution of R90, resc
 """
 charge_cloud_size(energy) = max(-0.03499440089585633 + 0.0003359462486002238 * energy, 0.01)
 
-function getδτ(firstloc)
-    get_signal(firstloc)
-    return setup().final_charge_size / setup().final_vel
+function getδτ(stp::SigGenSetup, firstloc)
+    get_signal!(stp, firstloc)
+    return stp.final_charge_size / stp.final_vel
 end
 
-function gausswindow(σ, nσ; step_time_out=setup().step_time_out)
+function gausswindow(σ::Real, nσ::Real, step_time_out::Real)
     # number of time steps to 1σ
     stepσ = σ / step_time_out
 
@@ -42,35 +42,77 @@ function gausswindow(σ, nσ; step_time_out=setup().step_time_out)
     return gaussian(n, 0.5 / nσ)
 end
 
-function pad_signal!(padvec, signal, wlen)
+function pad_signal!(padvec, signal, wl)
     sl = length(signal)
 
     # Padding only needed at end, since the beginning of the signal is ≈ 0
     padvec[1:sl] .= signal
-    padvec[sl+1:wlen+sl-1] .= signal[end]
+    padvec[sl+1:wl+sl-1] .= signal[end]
 
-    return padvec[1:sl+wlen-1]
+    return padvec[1:sl+wl-1]
 end
 
 function apply_group_effects(
-    signal,
-    δτ::Real,
-    padding::Bool,
-    padvec = (padding ? similar(signal, 2 * length(signal)) : similar(signal, 0)),
+    signal, δτ::Real, step_time_out::Real, padding::Bool,
+    padvec = (padding ? similar(signal) : similar(signal, 0))
 )
     # because δτ is FWHM
     σ = δτ / (2 * √(2 * log(2)))
 
     # gaussian window with 4σ to each edge
-    w = gausswindow(σ, 4)
+    w = gausswindow(σ, 4, step_time_out)
 
     if padding
         sl = length(signal)
         wl = length(w)
+
+		length(padvec) < sl + wl - 1 && resize!(padvec, sl + wl - 1)
+
         return conv(pad_signal!(padvec, signal, wl), w)[1:sl + wl - 1]
     else
         return conv(signal, w)
     end
+end
+
+function moving_average!(out, signal, width::Integer)
+	length(out) == length(signal) || error("ouptut vector must have same length as input vector")
+
+	# offsets from middle of window to beginning and end of window
+	bo = ceil(Int, width / 2)
+	eo = width - bo - 1
+
+	# Set beginning of output to the first moving mean
+	out[1:bo] .= sum(signal[1:width])
+
+	for i in bo+1:length(signal)-eo-1
+		out[i] = out[i-1] - signal[i-bo] + signal[i+eo+1]
+	end
+
+	out[end-eo:end] .= out[end-eo-1]
+	out ./= width
+	return out
+end
+
+function moving_average!(out, signal, width::Integer, n::Integer)
+	for _ in 1:floor(Int, n / 2)
+		moving_average!(out, signal, width)
+		moving_average!(signal, out, width)
+	end
+
+	if isodd(n)
+		moving_average!(out, signal, width)
+		return out
+	else
+		return signal
+	end
+end
+
+function moving_average(signal, width::Integer)
+	moving_average!(similar(signal), signal, width)
+end
+
+function moving_average(signal, width::Integer, n::Integer)
+	moving_average!(similar(signal), copy(signal), width, n)
 end
 
 function apply_electronics(signal) end
